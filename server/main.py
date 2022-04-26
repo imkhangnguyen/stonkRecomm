7b4ba79ff6e1cdbf07d73113f39e7ebbb925dfe3
Bill Sasi <billsasi@gmail.com>
2022-04-25 21:55:04 -0700

    added price info

--- server/main.py
+++ server/main.py
@@ -6,14 +6,15 @@ import queue_up as myQueue
 import sentiment_check as senCheck
 import symbol_check as symCheck
 import lib_import as lib
+import yfinance as yf
 
 
 def main(userInput):
-    
-
+    stockInfo = {}
+    for stock in userInput:
+        stockInfo[stock] = yf.Ticker(stock).info
 
     processedInput = symCheck.inputProcess(userInput)
-
     
     companyName = translate.match()
 
@@ -31,7 +32,6 @@ def main(userInput):
         sen_score, positive = senCheck.sen_check(df,sentiment_model)
         companyScore.append(sen_score)
         companyTweet.append(positive)
-        
        
     print("Companies: ")
     print(processedInput)
@@ -44,7 +44,7 @@ def main(userInput):
     companyTweetDict = dict(zip(processedInput, companyTweet))
     print("Top 3 Recommendation: ") 
 
-    return queue, companyTweetDict
+    return queue, companyTweetDict, stockInfo
 
 # userInput = ['TsLA','NvdA','AMD','GOOGl','AmZn']
 # queue,dict = main(userInput)
